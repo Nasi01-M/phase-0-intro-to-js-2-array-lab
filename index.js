304e1d1aa76722ce564bd25821aed4add05007b9
MICHELLE NASIRISI <wanjikunasirisi1@gmail.com>
2023-12-17 09:14:23 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Write your solution here!
+// index.js
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive Functions
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// Non-Destructive Functions
+function appendCat(name) {
+  return cats.concat(name);
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+module.exports = {
+  destructivelyAppendCat,
+  destructivelyPrependCat,
+  destructivelyRemoveLastCat,
+  destructivelyRemoveFirstCat,
+  appendCat,
+  prependCat,
+  removeLastCat,
+  removeFirstCat,
+};
